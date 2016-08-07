911933d9c6c6c8c00a696b373a773dc76dbe9c4f
Artur Siery <dotintegral@gmail.com>
2016-08-07 10:19:59 +0200

    Added writing css to output file

--- bem-module-loader.js
+++ bem-module-loader.js
@@ -1,17 +1,29 @@
+var promisify = require('promisify-node')
+var path = require('path')
 var loaderUtils = require("loader-utils");
 var selectorParser = require("./selector-parser")
+
+var fs = promisify('fs')
 
 module.exports = function (content) {
   var callback = this.async();
   var bemify = loaderUtils.stringifyRequest(this, require.resolve("./bemify.js"))
+  var options = loaderUtils.parseQuery(this.query)
+  var file = path.join(options.path, options.filename)
+  var parseSelectors = selectorParser.bind({}, content)
 
-  selectorParser(content)
-    .then(function (bemifyParams) {
-      var myContent = [
-        'var bemify = require(' + bemify + ')',
-        'var bem = bemify(' + JSON.stringify(bemifyParams) + ')',
-        'module.exports = bem'
-      ].join('\n')
-      callback(null, myContent)
-    })
+  var createModule = function (bemifyParams) {
+    var myContent = [
+      'var bemify = require(' + bemify + ')',
+      'var bem = bemify(' + JSON.stringify(bemifyParams) + ')',
+      'module.exports = bem'
+    ].join('\n')
+
+    callback(null, myContent)
+  }
+
+
+  fs.appendFile(file, content)
+    .then(parseSelectors)
+    .then(createModule)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: __dirname + '/build/',
-        filename: "bundle.js",
-        publicPath: 'http://localhost:8090/assets'
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-              plugins: ['transform-object-assign'],
-              presets: ['react', 'es2015']
-            }
-          }, {
-            test: /\.css$/,
-            loader: __dirname + '/bem-module-loader.js'
-          }
-        ]
-    },
-    plugins: [
-      new ExtractTextPlugin("styles.css")
-    ]
+  entry: './index.js',
+  output: {
+    path: __dirname + '/build/',
+    filename: "bundle.js",
+    publicPath: 'http://localhost:8090/assets'
+  },
+  module: {
+    loaders: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-object-assign'],
+          presets: ['react', 'es2015']
+        }
+      }, {
+        test: /\.css$/,
+        loader: __dirname + '/bem-module-loader.js',
+        query: {
+          path: __dirname + '/build/',
+          filename: 'bundle.css',
+        }
+      }]
+  }
 }
